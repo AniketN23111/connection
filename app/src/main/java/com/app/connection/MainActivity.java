3822aabb09a6abda278ddeea1fb0a4e618436755
AniketN23111 <aniketnarayankar3@gmail.com>
2023-03-23 21:54:47 +0530

    Add

--- app/src/main/java/com/app/connection/MainActivity.java
+++ app/src/main/java/com/app/connection/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.provider.Settings;
@@ -24,11 +26,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.Manifest;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.BroadcastReceiver;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -39,11 +44,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button button,logout_btn;
+    Button button,logout_btn,cam_btn;
     TextView textView,internet,battery,batteryper,latitudeTextView, longitTextView,timeStamp,IMEI;
     FusedLocationProviderClient mFusedLocationClient;
     FirebaseUser user;
@@ -52,10 +59,15 @@ public class MainActivity extends AppCompatActivity {
     String imei;
     Context context;
     Intent intent;
+    Uri uri;
     int level,status;
     boolean isCharging;
     Location location;
     TelephonyManager telephonyManager;
+    DatabaseReference reference;
+    FirebaseDatabase firebaseDatabase;
+    String inter,battery_per,battery_pow,lan,logi,times,user_mail,number,image;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,10 +75,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context=getApplicationContext();
         auth = FirebaseAuth.getInstance();
+        imageView=findViewById(R.id.image_view);
         button = findViewById(R.id.get_data);
         textView = findViewById(R.id.user_details);
         internet=findViewById(R.id.connection);
         battery=findViewById(R.id.battery);
+        cam_btn=findViewById(R.id.camera);
         batteryper=findViewById(R.id.batteryper);
         latitudeTextView = findViewById(R.id.latTextView);
         longitTextView = findViewById(R.id.lonTextView);
@@ -76,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
-
+try{
         int permisI=ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE);
         if(permisI==PackageManager.PERMISSION_GRANTED){
             telephonyManager=(TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
@@ -87,6 +101,11 @@ public class MainActivity extends AppCompatActivity {
         else{
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE},123);
         }
+        }
+catch (Exception e)
+{
+    imei="Not Available for this Device";
+}
 
         this.registerReceiver(this.broadcastReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
@@ -107,6 +126,57 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        cam_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+        final Handler handler=new Handler();
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                //TIMESTAMP
+                Long tsLong = System.currentTimeMillis()/1000;
+                String ts = tsLong.toString();
+                timeStamp.setText("TimeStamp:-"+ts);
+
+                //LOCATION
+                getLastLocation();
+                if (location == null) {
+                    requestNewLocationData();
+                } else {
+                    latitudeTextView.setText(location.getLatitude() + "");
+                    longitTextView.setText(location.getLongitude() + "");
+                }
+                //BATTERY
+                batteryper.setText("Battery Percentage: " + level);
+                if (isCharging) {
+                    battery.setText("Charger connected, Battery Charging..");
+
+                }
+                else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING) {
+                    battery.setText("Charger disconnected");
+                }
+                //INTERNET
+                if (isConnected()) {
+                    internet.setText("Internet Connected");
+                } else {
+                    internet.setText("Internet Not Connected");
+                }
+                //IMEI
+                IMEI.setText("IMEI NUMBER:-"+imei);
+                handler.postDelayed(this,90000);
+                getValues();
+
+            }
+        };
+        handler.post(runnable);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -140,10 +210,35 @@ public class MainActivity extends AppCompatActivity {
                 }
                 //IMEI
                 IMEI.setText("IMEI NUMBER:-"+imei);
+                getValues();
             }
         });
     }
 
+    private void getValues() {
+        inter=internet.getText().toString();
+        battery_per=batteryper.getText().toString();
+        battery_pow=battery.getText().toString();
+        lan=latitudeTextView.getText().toString();
+        logi=longitTextView.getText().toString();
+        times=timeStamp.getText().toString();
+        user_mail=user.getEmail();
+        number=imei;
+        image=String.valueOf(uri);
+Values values=new Values(inter,battery_per,battery_pow,lan,logi,times,user_mail,number,image);
+firebaseDatabase=FirebaseDatabase.getInstance();
+reference=firebaseDatabase.getReference("Values");
+reference.child("Value").setValue(values);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uri=data.getData();
+        imageView.setImageURI(uri);
+        image=String.valueOf(uri);
+    }
 
     //LOCATION
     @SuppressLint("MissingPermission")
--- app/src/main/java/com/app/connection/Values.java
+++ app/src/main/java/com/app/connection/Values.java
@@ -1,2 +1,91 @@
-package com.app.connection;public class Values {
+package com.app.connection;
+
+public class Values {
+String inter,battery_per,battery_pow,lan,logi,times,user_mail,number,image;
+Values(){}
+
+    public Values(String inter, String battery_per, String battery_pow, String lan, String logi, String times, String user_mail, String number,String image) {
+        this.inter = inter;
+        this.battery_per = battery_per;
+        this.battery_pow = battery_pow;
+        this.lan = lan;
+        this.logi = logi;
+        this.times = times;
+        this.user_mail = user_mail;
+        this.number = number;
+        this.image=image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getInter() {
+        return inter;
+    }
+
+    public void setInter(String inter) {
+        this.inter = inter;
+    }
+
+    public String getBattery_per() {
+        return battery_per;
+    }
+
+    public void setBattery_per(String battery_per) {
+        this.battery_per = battery_per;
+    }
+
+
+    public String getBattery_pow() {
+        return battery_pow;
+    }
+
+    public void setBattery_pow(String battery_pow) {
+        this.battery_pow = battery_pow;
+    }
+
+    public String getLan() {
+        return lan;
+    }
+
+    public void setLan(String lan) {
+        this.lan = lan;
+    }
+
+    public String getLogi() {
+        return logi;
+    }
+
+    public void setLogi(String logi) {
+        this.logi = logi;
+    }
+
+    public String getTimes() {
+        return times;
+    }
+
+    public void setTimes(String times) {
+        this.times = times;
+    }
+
+    public String getUser_mail() {
+        return user_mail;
+    }
+
+    public void setUser_mail(String user_mail) {
+        this.user_mail = user_mail;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
